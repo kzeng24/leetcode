2bb3ba48e155a0884e1db2d103757cfa0da88e7a
Katherine Zeng <52469441+kzeng24@users.noreply.github.com>
2023-01-02 18:45:19 -0500

    Create DeleteNodeInLinkedList.java

---
+++ Medium/DeleteNodeInLinkedList.java
@@ -0,0 +1,18 @@
+/*You are given the node to be deleted node. You will not be given access to the first node of head.
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory.*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
